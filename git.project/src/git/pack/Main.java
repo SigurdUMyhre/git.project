5d8a16cd73790ba29250badc7df32208e9f70691
Sigurd Ulimoen Myhre <sigumyhre@gmail.com>
2012-10-18 14:39:00 +0200

    This is a test

--- git.project/src/git/pack/Main.java
+++ git.project/src/git/pack/Main.java
@@ -8,6 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("This is git 2");
 		System.out.println("Adding a line");
+		System.out.println("Adding a new line");
 	}
 
 }
